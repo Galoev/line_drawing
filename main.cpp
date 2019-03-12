a841f9efe6b65504e1cdeb42edbcef3adc5f932d
Ilkin-Galoev <ilkinqaloev@gmail.com>
2019-03-12 14:25:20 +0300

    Добавили подсчет площади для случая length_x >= length_y и линии которая идет в верх

--- main.cpp
+++ main.cpp
@@ -6,8 +6,8 @@ using namespace std;
 
 struct Point
 {
-  int x;
-  int y;
+  float x;
+  float y;
 };
 
 
@@ -27,10 +27,11 @@ int main()
   memset(img, 255, width*height);
   
   Point p1, p2;
-  p1.x = 3;
-  p1.y = 15;
-  p2.x = 150;
-  p2.y = 150;
+  p1.x = 50;
+  p1.y = 90;
+  p2.x = 130;
+  p2.y = 60;
+  float thickness = 5.0;
   /*cin >> start.x;
    cin >> start.y;
    cin >> end.x;
@@ -51,6 +52,80 @@ int main()
     if (start.y > end.y){
       direction *= -1;
     }
+    
+    float h = (thickness / 2.0) / cos(atan2(length_y, length_x));
+    
+    for (int i = 1; i <= length_x - 1; i++)
+    {
+      float realIndex = i + 0.5; // пересчитать это для любых координат, а не только для целых
+      float y_f = (float)((realIndex-1)*length_y) / length_x + start.y;
+      float y_s = (float)(realIndex*length_y) / length_x + start.y;
+      
+      float maxLine = y_f > y_s ? y_f : y_s;
+      float minLine = y_f < y_s ? y_f : y_s;
+      
+      float maxUp = maxLine + h;
+      float minUp = minLine + h;
+      float maxDown = maxLine - h;
+      float minDown = minLine - h;
+      
+      int iMaxUp = maxUp;
+      int iMinUp = minUp;
+      int iMaxDown = ceil(maxDown);
+      int iMinDown = ceil(minDown);
+      
+      int x = ceil(start.x);
+      
+      int realX = i + 0.5;
+      //не совсем правльно закращивает
+      for (int y_filling = iMaxDown; y_filling <= iMinUp; y_filling++) {
+        img[y_filling * width + realX + x] = 0;
+      }
+      
+      if (iMaxUp == iMinUp) {
+        float a = minUp - iMinUp;
+        float b = maxUp - iMaxUp;
+        float S = (a + b)/2.0;
+        img[iMinUp * width + i + x] = pow(((1 - S)*gback + S*gline), 1 / 2.2f) * 255 + 0.5;
+      }
+      else {
+        float a = (maxUp - minUp);
+        float b = minUp;
+        float intersectionX = (iMaxUp - b)/a;
+        float S = (intersectionX * (iMaxUp-minUp))/2.0;
+        img[iMinUp * width + i + x] = pow((S*gback + (1 - S)*gline), 1 / 2.2f) * 255 + 0.5;
+        S = ((1 - intersectionX) * (maxUp - iMaxUp))/2.0;
+        img[iMaxUp * width + i + x] = pow(((1 - S)*gback + S*gline), 1 / 2.2f) * 255 + 0.5;
+      }
+      
+      if (iMaxDown == iMinDown) {
+        float a = iMinDown - minDown;
+        float b = iMaxDown - maxDown;
+        float S = (a + b)/2.0;
+        img[iMinDown * width + i + x] = pow(((1 - S)*gback + S*gline), 1 / 2.2f) * 255 + 0.5;
+
+      }
+      else {
+        float a = (maxDown - minDown);
+        float b = minDown;
+        float intersectionX = (iMinDown - b)/a;
+        float S = (intersectionX * (iMinDown-minDown))/2.0;
+        img[iMinDown * width + i + x] = pow(((1 - S)*gback + S*gline), 1 / 2.2f) * 255 + 0.5;
+        S = ((1 - intersectionX) * (maxDown - iMinDown))/2.0;
+        img[iMaxDown * width + i + x] = pow((S*gback + (1 - S)*gline), 1 / 2.2f) * 255 + 0.5;
+      }
+      
+      if (iMaxDown == iMinUp) {
+        
+      }
+      
+      
+      
+      
+      
+    }
+    
+    /*
     for (int i = 0; i <= length_x; i++)
     {
       float y_f = (float)(i*length_y) / length_x;
@@ -64,7 +139,8 @@ int main()
         img[(y + 1 * direction + start.y)*width + i + start.x] = pow(((1 - f)*gback + f*gline), 1 / 2.2f) * 255 + 0.5;
       }
     }
-  } else {
+  }
+  else {
     Point start = p1.y < p2.y ? p1 : p2;
     Point end = p1.y < p2.y ? p2 : p1;
     int direction = 1;
@@ -80,11 +156,12 @@ int main()
       if (f == 0)
       {
         img[(i+start.y)*width + x + start.x] = line;
-      } else {
+      }
+     else {
         img[(i+start.y)*width + x + start.x] = pow(((1 - f)*gline + f*gback), 1/2.2f)*255 + 0.5;
         img[(i+start.y)*width + x + 1*direction + start.x] = pow(((1 - f)*gback + f*gline), 1/2.2f)*255 + 0.5;
       }
-    }
+    }*/
   }
   
   writeImage("/Users/ilkin_galoev/Documents/8 semester/Видеоинформационные технологии/myImage.pgm", img, width, height);
